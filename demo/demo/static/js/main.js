97c3ab2d0ec34845771601aa59927ec913bbce2c
Anton Skubev <anton.skubev@academmedia.com>
2015-04-06 09:11:00 +0600

    add static

---
+++ demo/demo/static/js/main.js
@@ -0,0 +1,4 @@
+$('#sel1').on('change', function() {
+  alert( this.value ); // or $(this).val()
+});
+alert('GOVNO');
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url, include
 from .views import HomePageView, ScheduleView, SessionView
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from demo import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -17,4 +19,6 @@ urlpatterns = patterns('',
 urlpatterns += patterns('demo.views',
     url(r'^session/add/$', 'add_session', name='add_session'),
     url(r'^session/edit/(?P<id>.+)/$', 'edit_session', name='edit_session'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
